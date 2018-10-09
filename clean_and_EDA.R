e08b3f2c9efa088f7e5853ac1029d95022a9a097
Alejandra Gerosa <alegerosa@gmail.com>
2018-10-09 06:57:16 -0400

    Initial project files
    Upload .Rproj file and cleanup script

---
+++ clean_and_EDA.R
@@ -0,0 +1,73 @@
+library(tidyverse)
+#First, we load the data 
+## In order to work with all the dataframes in the directory, we'll create a vector with all their paths and then use lapply and read_csv to load all of them at once
+df_paths <- list.files("data", full.names = TRUE)
+list_of_dfs <- lapply(df_paths, read_csv)
+
+#We name the dataframes in the resulting list witht the file names they came from (minus the extension)
+df_names <- tolower(basename(substr(df_paths, 1, nchar(df_paths)-4)))
+names(list_of_dfs) <- df_names
+
+#Let's see what we have
+str(list_of_dfs)
+
+#Rename columns for lower case and avoiding spaces, to make it easier to call them
+for (i in 1:length(list_of_dfs)) {
+  list_of_dfs[[i]] <- rename_all(list_of_dfs[[i]], funs(gsub(" ", "_", .)))
+  list_of_dfs[[i]] <- rename_all(list_of_dfs[[i]], funs(tolower(.)))
+} 
+ 
+str(list_of_dfs)
+
+#Bring 6 separate dataframes to my environment, to make it easier to call them
+list2env(list_of_dfs, .GlobalEnv)
+
+#Look at each dataframe to see what additional cleanup is needed
+
+#donations
+#turn the donation_included_optional_donation field from character Yes/No to logicals (first I check that there are no Maybes or other values)
+table(donations$donation_included_optional_donation)
+donations$donation_included_optional_donation <- donations$donation_included_optional_donation == "Yes"
+
+#donors
+#turn donor_is_teacher from character to logical (checking first that all values are yes or no)
+table(donors$donor_is_teacher)
+donors$donor_is_teacher <- donors$donor_is_teacher == "Yes"
+
+#projects
+glimpse(projects)
+#project_type seems to be a factor. Check and if appropriate change to factor
+table(projects$project_type)
+projects$project_type <- as.factor(projects$project_type)
+#same with a few other fields
+table(projects$project_subject_category_tree)
+table(projects$project_subject_subcategory_tree)
+#these two are interesting and it seems like I might want to play with the strings/do further wrangling at some point, so I'll leave as character for now
+table(projects$project_grade_level_category)
+projects$project_grade_level_category <- as.factor(projects$project_grade_level_category)
+table(projects$project_resource_category)
+projects$project_resource_category <- as.factor(projects$project_resource_category)
+table(projects$project_current_status)
+projects$project_current_status <- as.factor(projects$project_current_status)
+
+#resources
+glimpse(resources)
+#examined resource_vendor_name, decided to keep it as character given the amount of different vendors. might change my mind later
+table(resources$resource_vendor_name)
+
+#schools
+glimpse(schools)
+table(schools$school_metro_type)
+schools$school_metro_type <- as.factor(schools$school_metro_type)
+
+#teachers
+glimpse(teachers)
+table(teachers$teacher_prefix)
+teachers$teacher_prefix <- as.factor(teachers$teacher_prefix)
+
+##NOW WE CAN START EDA
+summary(donations)
+#ggplot(donations, aes(x= project_id)) +
+#  geom_bar()
+# plot(donations$project_id)
+# plot(donations)
